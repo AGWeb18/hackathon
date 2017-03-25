526500370001ec6316c1c80b5493474cfb8bf373
Mnaranjo <mnaranjo@360insights.com>
2017-03-25 11:55:17 -0400

    Updated Messages

--- OddJobb/www/js/controllers/message.js
+++ OddJobb/www/js/controllers/message.js
@@ -1,25 +1,46 @@
 //Each message Page
 myApp.onPageInit('indMsg', function (page) {
-    var queryParams = page.query["messageId"];
-    var title = document.getElementById("currentPerson");
-    title.innerHTML = queryParams;
-    messagesMain(queryParams);
+  //deleteAllMsgs();
+  var personName = page.query["personName"];
+  var email = page.query["email"];
+  var title = document.getElementById("currentPerson");
+  //Change title of message
+  title.innerHTML = personName;
+  //Get message of people using their email
+  getMessages(email, true);
+  sendMessage(personName, email);
 })
 
 //Messages Page
 myApp.onPageInit('messages', function (page) {
+  var peopleTemplate = '<li>' +
+  '<a href="indMsg.html?personName={{personName}}&email={{email}}" class="item-link item-content">' +
+  '<div>{{item}}: {{messagePreview}}</div>' +
+  '</a>' +
+  '</div>' +
+  '</li>';
 
-    //Will be reading this from the database
-    const people = [{ item: "Mateo" }, { item: "Holly" }, { item: "Anthony" }, { item: "Haowei" }];
 
-    const myListOfPeople = myApp.virtualList('.list-block.virtual-list', {
-        items: people,
-        template:
-        '<li>' +
-        '<a href="indMsg.html?messageId={{item}}" class="item-link item-content">' +
-        '<div>{{item}}</div>' +
-        '</a>' +
-        '</div>' +
-        '</li>'
-    });
+  //Will be reading this from the database
+  //Get list of users
+  $.get('http://oddjobbackend.herokuapp.com/users', function(data) {
+    const people = [];
+    for(var i = 0; i < data.length; i++){
+      var currentP = data[i];
+      console.log(currentP);
+      getMessages(currentP.email, false, function(msgs){
+        var msg = "Test Message Test Message Test Message";
+        if(msgs.length > 0){
+          msg = msgs[msgs.length - 1].message;
+        }
+        people.push({item:currentP.firstName, messagePreview:msg.substring(0, 20).substring(0, 10), email:currentP.email});
+        const myListOfPeople = myApp.virtualList('.list-block.virtual-list', {
+          items: people,
+          template: peopleTemplate
+        });
+      });
+
+    }
+
+  });
 })
--- OddJobb/www/js/messages.js
+++ OddJobb/www/js/messages.js
@@ -1,84 +1,114 @@
 // JavaScript source code
 //Js file that deal with the messages
 
+var prevMsgs = {};
+
 
 var msgTemplate = '{{#if day}}' +
     '<div class="messages-date">{{day}} {{#if time}}, <span>{{time}}</span>{{/if}}</div>' +
     '{{/if}}' +
-    '<div class="message message-{{type}} {{id}} hidden {{#if hasImage}}message-pic{{/if}} {{#if avatar}}message-with-avatar{{/if}} {{#if position}}message-appear-from-{{position}}{{/if}}">' +
-    '{{#if name}}<div class="message-name {{id}} hidden">{{name}}</div>{{/if}}' +
-    '<div class="message-text {{id}}">{{text}}{{#if date}}<div class="message-date hidden">{{date}}</div>{{/if}}</div>' +
-    '{{#if avatar}}<div class="message-avatar" style="background-image:url({{avatar}})"></div>{{/if}}' +
-    '{{#if label}}<div class="message-label">{{label}}</div>{{/if}}' +
+    '<div class="message message-{{type}}  {{id}} {{#if hasImage}}message-pic{{/if}} {{#if avatar}}message-with-avatar{{/if}} {{#if position}}message-appear-from-{{position}}{{/if}}">' +
+    '{{#if name}}<div class="message-name {{id}}">{{name}}</div>{{/if}}' +
+    '<div class="message-text {{id}}">{{text}}{{#if date}}<div class="message-date {{id}}">{{date}}</div>{{/if}}</div>' +
+    '{{#if avatar}}<div class="message-avatar {{id}}" style="background-image:url({{avatar}})"></div>{{/if}}' +
+    '{{#if label}}<div class="message-label {{id}}">{{label}}</div>{{/if}}' +
     '</div>';
 
 var error = "";
 
-function messagesMain(queryParams) {
-    var msgHandler = new Framework7();
-    var $$ = Dom7;
+function sendMessage(personName, email) {
+  var msgHandler = new Framework7();
+  var $$ = Dom7;
 
-    var messages = msgHandler.messages('.messages', { messageTemplate: msgTemplate });
-    var msgBar = msgHandler.messagebar('.messagebar');
-    var currentMsg = "";
+  var messages = msgHandler.messages('.messages', { messageTemplate: msgTemplate });
+  var msgBar = msgHandler.messagebar('.messagebar');
+  var currentMsg = "";
 
-    //Have to read from api to know which user to read from
-    //console.log(queryParams);
-    var prevMessages = loadMessages(queryParams);
+  //Really only applicable to desktop version
+  $$('.messagebar').on('keypress', function (e) {
+      if (e.keyCode === 13) {
+          sendMsg();
+      }
+  })
+  $$('.messagebar').on('click', function () {
+      sendMsg();
+  })
 
-    //Really only applicable to desktop version
-    $$('.messagebar').on('keypress', function (e) {
-        if (e.keyCode === 13) {
-            sendMsg();
-        }
-    })
+  var sendMsg = function () {
+      var msgId = newId(1000);
+      currentMsg = msgBar.value().trim();
+      msgBar.clear();
+      messages.addMessage({
+          text: currentMsg,
+          name: personName,
+          id: msgId
+      }, "append", true);
 
-    $$('.messagebar').on('click', function () {
-        sendMsg();
-    })
-
-    var sendMsg = function () {
-        currentMsg = msgBar.value().trim();
-        msgBar.clear();
-
-        messages.addMessage({
-            text: currentMsg,
-            name: "Mateo",
-            id: "Mateo"
-        }, "append", true);
-   
-        console.log("message sent"); 
-    }
-
-    printErr(error);
-
+      $.post("http://oddjobbackend.herokuapp.com/newMessage?email=" + email + "&name=" + personName + "&message="+ currentMsg + "&personSent=(PERSONSENT)&id="+ msgId, function(data){
+        console.log("Messgae submitted " + data);
+      });
+  }
+  printErr(error);
 };
 
-//Load messages of a particular person.
-var loadMessages = function (person) {
-    allMsgs = $$("." + person);
-    console.log(allMsgs);
-    allMsgs.addClass("visible");
-    allMsgs.removeClass("hidden");
+var deleteAllMsgs = function(){
+  getMessages(undefined, function(msgs){
+    console.log(msgs);
+    var curMsgs = {};
+    for(var t = 0; t < msgs.length; t++){
+      curMsgs = msgs[t];
+      console.log(msgs[t]);
+      $.del("http://oddjobbackend.herokuapp.com/deleteMessage/" + curMsg["_id"], function(data){
+        console.log("Message " + curMsgs["id"] + " deleted.");
+      });
+    }
+  });
 
-    /*
-    var msgs = $$("." + person);
-    if (msgs === null) {
-        error = "Invalid Person Called";
-        return;
-    }
-    var people = [];
-    for (var i = 0; i < msgs.length; i++) {
-        var current = msgs[i];
-        if (people.indexOf(current.innerHTML) === -1) {
-            people.push(current);
-        }
-    }
-    //console.log(people);
-    return people;
-    */
 }
 
+//Load messages of a particular person.
+var getMessages = function (personEmail, seeMessages, callback) {
+  var msgHandler = new Framework7();
+  var $$ = Dom7;
+
+  var allMessages = msgHandler.messages('.messages', { messageTemplate: msgTemplate });
+  var msgBar = msgHandler.messagebar('.messagebar');
+  var currentM = {};
+  var pMsgs = [];
+
+  $.get("http://oddjobbackend.herokuapp.com/messages", function(data){
+    for(var i = 0; i < data.length; i++){
+      var currentM = data[i];
+      //Get all messages
+      if(personEmail === undefined){
+        pMsgs.push(currentM);
+      }
+      else if(currentM.email === personEmail && currentM.message !== "()"){
+        pMsgs.push(currentM);
+        //console.log(currentM);
+        if(seeMessages){
+          allMessages.addMessage({
+            text: currentM.message,
+            name: currentM.name,
+            id: currentM.id
+          }, "append", true);
+          console.log("Message Added");
+        }
+      }
+    }
+    if(callback){
+      callback(pMsgs);
+      return pMsgs;
+    }
+
+  });
+  return pMsgs;
+}
+
+var newId = function(range){
+  var res = Math.floor(Math.random() * range);
+  return res;
+}
 
 var printErr = function(err){
     console.log(err);
